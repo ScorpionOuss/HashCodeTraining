d86a713dbcc183a7262652009be017f282adc645
hakim <hakim.ouhida@grenoble-inp.org>
2022-11-26 10:19:55 +0000

    evaluate function

--- Rides/common_library.py
+++ Rides/common_library.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python
+
+def evaluate(allocations: list[list[Ride]]):
+    total_score = 0
+    for rides in allocations:
+        car_score = 0
+        car_pos = (0, 0)
+        timestamp = 0
+        for ride in rides:
+            timestamp += abs(car_pos[0] - ride.a) + abs(car_pos[1] - ride.b)
+            on_time = timestamp == ride.s
+            timestamp += abs(ride.a - ride.x) + abs(ride.b - ride.y)
+            if timestamp < T:
+                car_score += 1 + int(on_time) * B
+            else:
+                break
+        total_score += car_score
+    return car_score
+        
