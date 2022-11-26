0392533ec2b8682232926a5ab89703ba9fc2d5b2
Oussama KADDAMI <kaddami@kth.ug.se>
2022-11-26 12:02:16 +0100

    Pushing with arrangements

--- Rides/brute_force.py
+++ Rides/brute_force.py
@@ -25,6 +25,7 @@ def find_optimum():
             test_score = evaluate(fleets_list)
             # May need a copy
             if test_score > current_score:
+                current_score = test_score
                 current_solution = deepcopy(fleets_list)
         for i in range(F):
             fleets_list[i].append(Rides[r])
@@ -43,4 +44,38 @@ def find_optimum():
             fleets_list[i].pop()
     
     recursive_enumeration(0)
+    return current_solution
+
+
+def find_optimum_with_arrangements():
+    """
+    Return the optimal solution using the brute force approach
+    """
+    current_solution = None
+    current_score = 0
+
+    fleets_list = [[]for _ in range(F)]
+    def recursive_enumeration(r):
+
+        visited_rides = set()
+
+        def recursive_enumeration():
+            if len(visited_rides) == R:
+                # We treated all rides
+                test_solution = evaluate(fleets_list)
+                if test_solution > current_score:
+                    current_score = test_solution
+                    current_solution= deepcopy(fleets_list)
+
+            for i in range(R):
+                if i not in visited_rides:
+                    visited_rides.add(i)
+                    for fleet in range(F):
+                        fleets_list[fleet].append(Rides[i])
+                        # Evaluate the sub-solution
+                        recursive_enumeration()
+                        fleets_list[fleet].pop()
+                    visited_rides.remove(i)
+
+    recursive_enumeration()
     return current_solution
