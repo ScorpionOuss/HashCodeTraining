a4504d208529d650f9c5e46b4bfbb844dfcbb6dd
hakim <hakim.ouhida@grenoble-inp.org>
2022-12-17 11:14:08 +0000

    check if solution is valid

--- streaming/evaluate.py
+++ streaming/evaluate.py
@@ -1,17 +1,26 @@
-from readData import Endpoints, Requests
+from readData import Request
+from readData import Endpoints, Requests, Sizes, X
 
 
-def get_lowest_latency(request, solution):
+def is_valid(solution: dict):
+    for videos in solution.values():
+        s = sum(Sizes[video] for video in videos)
+        if s > X:
+            return False
+    return True
+
+def get_lowest_latency(request: Request, solution: dict):
     endpoint = Endpoints[request.idEndpoint]
     latencies = []
     for connection in endpoint.cacheConnections:
-        if request.video in solution.get(connection.id, []):
+        if request.idVideo in solution.get(connection.id, []):
             latencies.append(connection.latency)
         else:
             latencies.append(float("+inf"))
     return min(endpoint.latencyDC, latencies)
 
 def evaluate(solution):
+    assert is_valid(solution), "Solution invalid"
     total_latency = 0
     for id, request in enumerate(Requests):
         l = get_lowest_latency(id, solution)
